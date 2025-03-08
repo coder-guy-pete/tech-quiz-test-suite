bfa913e319bcb8c4f97ae2438a8995427e253964
Peter Hintze <hintze.peter@gmail.com>
2025-03-08 12:06:40 -0500

    cypress initial setup

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from "cypress";
+import viteConfig from "./vite.config";
 
 export default defineConfig({
   component: {
     devServer: {
       framework: "react",
       bundler: "vite",
+      viteConfig,
     },
     specPattern: "cypress/component/**/*.cy.{ts,tsx}",
+  },
+
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
